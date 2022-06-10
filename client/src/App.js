2b487ef2df192320410a02b47f327c73f693931a
Umer Farooq <98796369+umerfarooq777@users.noreply.github.com>
2022-06-10 01:16:13 -0700

    api integrated

--- client/src/App.js
+++ client/src/App.js
@@ -1,44 +1,74 @@
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import useGeoLocation from "./hooks/useGeoLocation";
-import { useEffect,useState } from "react";
+import { useEffect, useState } from "react";
 
 const weather_api_key = "7d2b0196a91d8549991cb66ad004cd9a";
-const base_weather_url ="https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}";
+const base_weather_url =
+  "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}";
+
 function App() {
-  const [long1, setLong1] = useState("");
-  const [lat1, setLat1] = useState("");
   const [weather, setweather] = useState(null);
+  const [lat, setLat] = useState(null);
+  const [lng, setLng] = useState(null);
+  const [status, setStatus] = useState(null);
 
-
+  // console.log("🚀 ~ file: App.js ~ line 55 ~ getPokemon ~ res", weather.wind? alert("wind hai"):alert("wind nahi hai"));
   const location = useGeoLocation();
-  
-  const latitude = location.loaded
-    ? JSON.stringify(location.coordinates.lat)
-    : "Location data not available yet.";
-  const longitude = location.loaded
-    ? JSON.stringify(location.coordinates.lng)
-    : "Location data not available yet.";
-  
-  
-  
-  useEffect(() => {
-    const fetchAPI = async () => {
-      try {
-          const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${weather_api_key}`;
-          const response = await fetch(url);
-          const responseJSON = await response.json();
-          if(response.ok){
-            setweather(responseJSON);
-            console.log(weather)
-          }
-        } catch (error) {
-          console.log(error);
-        }
-      };
 
-      fetchAPI();
-    }, [longitude]);
+  // const getLocation = () => {
+  //   const latitude = location.loaded
+  //     ? JSON.stringify(location.coordinates.lat)
+  //     : "Location data not available yet.";
+  //     setLat(latitude)
+
+  //     const longitude = location.loaded
+  //     ? JSON.stringify(location.coordinates.lng)
+  //     : "Location data not available yet.";
+  //     setLng(longitude)
+
+  // };
+
+  const getLocation = () => {
+    // const getLocation = () => {
+    try {
+      setStatus("Locating...");
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          setStatus("hogya");
+          setLat(position.coords.latitude);
+          setLng(position.coords.longitude);
+        },
+        () => {
+          setStatus("Unable to retrieve your location");
+        }
+      );
+    } catch {
+      setStatus("Geolocation is not supported by your browser");
+    }
+    // }
+  };
+
+  useEffect(() => {
+    const fetchPokemon = () => {
+      const getPokemon = async () => {
+        getLocation();
+        console.log(
+          "🚀 ~ file: App.js ~ line 107 ~ getPokemon ~ getLocation",
+          lng,
+          lat,
+          status
+        );
+        // const url = `https://api.openweathermap.org/data/2.5/weather?lat=24.8686444&lon=67.0821597&appid=7d2b0196a91d8549991cb66ad004cd9a`;
+        const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=7d2b0196a91d8549991cb66ad004cd9a`;
+        const data = await fetch(url);
+        const res = await data.json();
+        setweather(res);
+      };
+      getPokemon();
+    };
+    fetchPokemon();
+  }, [lng && lat && status === "hogya"]);
 
   return (
     <div>
@@ -56,32 +86,54 @@ function App() {
             <h1>30 C</h1>
           </div>
 
-          {/* <div className="message ">
-            <p>
-              {latitude}
-              <br />
-              {longitude}
-            </p>
-          </div> */}
+          {weather && weather.coord ? (
+            <div className="message ">
+              <p>
+                {weather.coord.lon}
+                <br />
+              </p>
+            </div>
+          ) : null}
 
-          <div className="prop-cont">
-            <div className="tempdiv2">
-              <h1>...</h1>
-              <p>Feels Like</p>
+          {weather && weather.wind ? (
+            <div className="prop-cont">
+              <div className="tempdiv2">
+                <h1>{weather.main.feels_like}</h1>
+                <p>Feels Like</p>
+              </div>
+              <div className="tempdiv2">
+                <h1>{weather.main.humidity}</h1>
+                <p>Humidity</p>
+              </div>
+              <div className="tempdiv2">
+                <h1>{`${weather.wind.speed} /${weather.wind.deg}`}</h1>
+                <p>Wind Speed</p>
+              </div>
+              <div className="tempdiv2">
+                <h1>{weather.main.feels_like}</h1>
+                <p>Pressure</p>
+              </div>
             </div>
-            <div className="tempdiv2">
-              <h1>...</h1>
-              <p>Humidity</p>
+          ) : (
+            <div className="prop-cont">
+              <div className="tempdiv2">
+                <h1>...</h1>
+                <p>Feels Like</p>
+              </div>
+              <div className="tempdiv2">
+                <h1>...</h1>
+                <p>Humidity</p>
+              </div>
+              <div className="tempdiv2">
+                <h1>...</h1>
+                <p>Wind Speed</p>
+              </div>
+              <div className="tempdiv2">
+                <h1>...</h1>
+                <p>Pressure</p>
+              </div>
             </div>
-            <div className="tempdiv2">
-              <h1>...</h1>
-              <p>Wind Speed</p>
-            </div>
-            <div className="tempdiv2">
-              <h1>...</h1>
-              <p>Pressure</p>
-            </div>
-          </div>
+          )}
         </div>
       </div>
     </div>
